834738b44cf050f03049dcd4c91ec399d4c594a9
Muvvalakaran <karanmuvvala@yahoo.in>
2025-09-18 17:52:15 -0600

    WIP: Added correct code for synthesizing grdiworld str. Adding code to implement wait action for reach objectives.

--- test_gridworld.py
+++ test_gridworld.py
@@ -1,28 +1,38 @@
 import math
 import warnings
 
-from typing import List, Tuple
+from enum import Enum
+from functools import reduce
+from typing import List, Tuple, Dict
 from collections import defaultdict
 
 from bidict import bidict
 from cudd import Cudd, ADD, BDD
+
+class Moves(Enum):
+    NORTH = (-1, 0)
+    SOUTH = (1, 0)
+    WEST = (0, -1)
+    EAST = (0, 1)
+
 
 class AddGridWorld:
 
     def __init__(self, rows: int, columns: int, init: tuple, goal: tuple):
         self.rows = rows
         self.columns = columns
-        self.robot_actions: set = frozenset(['east', 'west', 'north', 'south'])
-        self.env_actions: set = frozenset(['north-east', 'north-west', 'south-east', 'south-west'])
-        self.init = init if (0 < init[0] < self.rows and 0 < init[1] < self.columns) else warnings.warn("Make sure init state in within the bounds of the gridworld.")
-        self.goal = goal if (0 < goal[0] < self.rows and 0 < goal[1] < self.columns) else warnings.warn("Make sure goal state in within the bounds of the gridworld.")
+        self.robot_actions: set = frozenset(['EAST', 'WEST', 'NORTH', 'SOUTH'])
+        self.env_actions: List[str] = ['north-east', 'north-west', 'south-east', 'south-west', 'no-int']
+        self.init = init if (0 <= init[0] < self.rows and 0 <= init[1] < self.columns) else warnings.warn("Make sure init state in within the bounds of the gridworld.")
+        self.goal = goal if (0 <= goal[0] < self.rows and 0 <= goal[1] < self.columns) else warnings.warn("Make sure goal state in within the bounds of the gridworld.")
         self.manager: Cudd = Cudd()
-        self.iVars: List[ADD] = self.create_input_cube()
-        self.oVars: List[ADD] = self.create_output_cube()
+        self.iVars: List[ADD] = self.create_input_vars()
+        self.oVars: List[ADD] = self.create_output_vars()
         self.xVars, self.yVars = self.create_latches()
         self.xVars_bdd: List[BDD] = [var.bddPattern() for var in self.xVars]
         self.yVars_bdd: List[BDD] = [var.bddPattern() for var in self.yVars]
-        self.latches  = self.xVars + self.yVars
+        self.latches: List[ADD]  = self.xVars + self.yVars
+        self.latches_bdd: List[BDD] = [var.bddPattern() for var in self.latches]
 
         self.winning_states: ADD = defaultdict(lambda: self.manager.plusInfinity())
 
@@ -41,17 +51,23 @@ class AddGridWorld:
         # create mapping for robot and env actions
         self.create_action_map()
 
+        self.iVars_cube: ADD = reduce(lambda x, y: x & y, self.iVars)
+        self.oVars_cube: ADD = reduce(lambda x, y: x & y, self.oVars)
+
         # create a list that will hold the funcitonal ADDs.
         self.transition_relation = {var.bddPattern().__str__(): self.manager.addZero() for var in self.latches}
     
-    def create_input_cube(self) -> List[ADD]:
+    def create_input_vars(self) -> List[ADD]:
         varsize = self.manager.size()
-        iVars: List[ADD] =  [self.manager.addVar(k + varsize , 'i' + str(k)) for k in range(2)]
+        # We not create the no-int action
+        iVars_size = math.ceil(math.log2(len(self.env_actions))) - 1
+        iVars: List[ADD] =  [self.manager.addVar(k + varsize , 'i' + str(k)) for k in range(iVars_size)]
         return iVars
     
-    def create_output_cube(self) -> List[ADD]:
+    def create_output_vars(self) -> List[ADD]:
         varsize = self.manager.size()
-        oVars: List[ADD] =  [self.manager.addVar(k + varsize, 'o' + str(k)) for k in range(2)]
+        oVars_size = math.ceil(math.log2(len(self.robot_actions)))
+        oVars: List[ADD] =  [self.manager.addVar(k + varsize, 'o' + str(k)) for k in range(oVars_size)]
         return oVars
     
     def create_latches(self):
@@ -73,13 +89,13 @@ class AddGridWorld:
         """
         valid_actions = set()
         if rPos + 1 < self.rows:
-            valid_actions.add('south')
+            valid_actions.add('SOUTH')
         if rPos - 1 >= 0:
-            valid_actions.add('north')
+            valid_actions.add('NORTH')
         if cPos + 1 < self.columns:
-            valid_actions.add('east')
+            valid_actions.add('EAST')
         if cPos - 1 >= 0:
-            valid_actions.add('west')
+            valid_actions.add('WEST')
         
         return valid_actions
     
@@ -87,27 +103,31 @@ class AddGridWorld:
         """
          Returns a list valid env actions you can take given row and column position
         """
-        valid_actions = set()
-        if robot_act == 'north' and cPos + 1 < self.columns:
+        # env can always choose to not intervene
+        valid_actions = set({'no-int'})
+        if robot_act == 'NORTH' and cPos + 1 < self.columns:
             valid_actions.add('north-east')
-        if robot_act == 'south'and cPos - 1 >= 0:
+        if robot_act == 'SOUTH'and cPos - 1 >= 0:
             valid_actions.add('south-west')
-        if robot_act == 'east' and rPos + 1 < self.rows:
+        if robot_act == 'EAST' and rPos + 1 < self.rows:
             valid_actions.add('south-east')
-        if robot_act == 'west' and rPos - 1 >= 0:
+        if robot_act == 'WEST' and rPos - 1 >= 0:
             valid_actions.add('north-west')
         
         return valid_actions
 
-    def get_next_state(self, rPos: int, cPos: int, act: str) -> Tuple[int, int]:
-        if act == 'north-east':
+    def get_next_state(self, rPos: int, cPos: int, eAct: str, rAct: str) -> Tuple[int, int]:
+        assert eAct in self.env_actions, "Make sure the action is valid"
+        if eAct == 'north-east':
             return rPos - 1, cPos + 1
-        elif act == "north-west":
+        elif eAct == "north-west":
             return rPos - 1, cPos - 1
-        elif act == "south-east":
+        elif eAct == "south-east":
             return rPos + 1, cPos + 1
-        elif act == "south-west":
+        elif eAct == "south-west":
             return rPos + 1, cPos - 1
+        elif eAct == "no-int":
+            return rPos + Moves[rAct].value[0], cPos + Moves[rAct].value[1]
     
     def create_xVar_map(self) -> None:
         for r in range(self.rows):
@@ -126,8 +146,10 @@ class AddGridWorld:
             self.rAction_map[ract] = rbit_str
         
         for eidx, eact in enumerate(self.env_actions):
-            ebit_str = f"{eidx:0{len(self.iVars)}b}"
-            self.eAction_map[eact] = ebit_str
+            # skip the no-int action
+            if eact != "no-int": 
+                ebit_str = f"{eidx:0{len(self.iVars)}b}"
+                self.eAction_map[eact] = ebit_str
 
     def cube_to_add(self, cube: str, vars_list: List) -> ADD:
         assert len(cube) == len(vars_list), "Make sure the length of the cube is the same as the number of latches"
@@ -135,7 +157,21 @@ class AddGridWorld:
         for idx, val in enumerate(cube):
             add &= vars_list[idx] if val == '1' else ~vars_list[idx]
         return add
-    
+
+    def get_no_int_cube(self, valid_env_actions: List[str]) -> ADD:
+        """
+         Given a set of valid human moves, return the a cube that represents no intervention.
+
+         If valid env move = {south-west} then no intervention = ~(south-west)
+         If valid env move = {north-east, south-west} then no intervention = ~(north-east | south-west)
+         If valid env move = {} then no intervention = 1
+        """
+        valid_env_cube_strings = [self.eAction_map[i] for i in valid_env_actions if i != "no-int"]
+        no_human_int = self.manager.addOne()
+        if len(valid_env_cube_strings) > 0:
+            valid_env_cubes = [self.cube_to_add(i, self.iVars) for i in valid_env_cube_strings]
+            no_human_int = ~reduce(lambda x, y: x | y, valid_env_cubes)
+        return no_human_int
 
     def create_transition_relation(self):
         """
@@ -153,23 +189,40 @@ class AddGridWorld:
                     # get valied env actions for every robot act and gridworld position (r,c)
                     valid_env_actions = self.get_valid_env_transitions(rPos=r, cPos=c, robot_act=rAct)
                     rAct_cube: str = self.rAction_map[rAct]
+                    no_human_int: ADD = self.get_no_int_cube(valid_env_actions=valid_env_actions)
+                    
                     for eAct in valid_env_actions:
                         # create the transition relation
-                        next_rPos, next_cPos = self.get_next_state(rPos=r, cPos=c, act=eAct)
+                        next_rPos, next_cPos = self.get_next_state(rPos=r, cPos=c, eAct=eAct, rAct=rAct)
+                        eAct_cube_string: str = self.eAction_map.get(eAct, None)
+                        eAct_cube: ADD = no_human_int
+                        if isinstance(eAct_cube_string, str):
+                            eAct_cube = self.cube_to_add(eAct_cube_string, self.iVars)
                         
-                        eAct_cube: str = self.eAction_map[eAct]
                         # if next state var is positive 
                         for idx, prime_rVar in enumerate(self.xVar_map[next_rPos]):
                             if prime_rVar == '1':
-                                self.transition_relation[self.xVars_bdd[idx].__str__()] |= rVar_add & self.cube_to_add(rAct_cube, self.oVars) & self.cube_to_add(eAct_cube, self.iVars)
+                                self.transition_relation[self.xVars_bdd[idx].__str__()] |= rVar_add & self.cube_to_add(rAct_cube, self.oVars) & eAct_cube
                         
                         for idx, prime_cVar in enumerate(self.yVar_map[next_cPos]):
                             if prime_cVar == '1':
-                                self.transition_relation[self.yVars_bdd[idx].__str__()] |= cVar_add & self.cube_to_add(rAct_cube, self.oVars) & self.cube_to_add(eAct_cube, self.iVars)
+                                self.transition_relation[self.yVars_bdd[idx].__str__()] |= cVar_add & self.cube_to_add(rAct_cube, self.oVars) & eAct_cube
     
+
+    def get_buckets_of_BDD(self, max_interval_val: int, layer: int) -> Dict[int, BDD]:
+        _win_state_bucket: Dict[int, BDD] = defaultdict(lambda: self.manager.bddZero())
+        for sval in range(max_interval_val + 1):
+            # get the states with state value equal to sval and store them in their respective bukcets
+            win_sval: BDD = self.winning_states[layer].bddInterval(sval, sval)
+            
+            if not win_sval.isZero():
+                _win_state_bucket[sval] |= win_sval
+        
+        return _win_state_bucket
+
     
-    def preimage(self, From: BDD) -> BDD:
-        return From.vectorCompose(self.latches, self.transition_relation)
+    def preimage(self, ts_action: List[BDD], From: BDD) -> BDD:
+        return From.vectorCompose(self.latches_bdd, ts_action)
     
     def solve(self):
         """
@@ -183,31 +236,83 @@ class AddGridWorld:
             # add the state to winning region
             # if fixpoint then break
         """
-        goal = self.goal.ite(self.manager.addZero(), self.manager.plusInfinity())
+        # strategy - optimal (state & robot-action) pair stored in the ADD
+        strategy: ADD  = self.manager.plusInfinity()
+        
+        # initialize goal state with 0 state value and add it to the winnign regiom
+        goal = self.goal_latch.ite(self.manager.addZero(), self.manager.plusInfinity())
         self.winning_states[0] |= self.winning_states[0].min(goal)
+        strategy = strategy.min(goal)
+        
+        # intialize the iteration counter and hardcode the action cost
         layer = 0
+        c_max = 1 # hardcoded for now. Make it general in future
+        act_val = 1 # hardcoded action cost for now. Make it general in future
+
+        # preprocess the transition relation to be list of functional BDDs as CUDD's vectorCompsoe only accepts list of functional BDDs
+        partitioned_tr_bdd = [tr.bddPattern() for tr in self.transition_relation.values()]
         while True:
-            if self.winning_states[layer].compare(self.winning_states[layer - 1]):
-                break
+            # if oyu reach a fix point then break
+            if self.winning_states[layer].compare(self.winning_states[layer - 1], 2):
+                print("**************************Reached fixpoint**************************")
+                if self.init_latch & self.winning_states[layer] != self.manager.plusInfinity():
+                    init_val: int = list((self.init_latch & self.winning_states[layer]).generate_cubes())[0][1]
+                    print(f"A Winning Strategy Exists!!. The State value is {init_val}")
+                    return strategy
+                return
 
             # if print_layers:
             print(f"**************************Layer: {layer}**************************")
-            # compute preimage
-            pre: BDD = self.preimage(From=self.winning_states[layer])
 
+            # convert the winning states into buckets of BDD
+            max_interval_val = layer * c_max
+            win_state_bucket: Dict[int, BDD] = self.get_buckets_of_BDD(max_interval_val, layer)
 
-            # take max over env actions
-            # take min over sys actions
+            pre_buckets: Dict[ADD] = defaultdict(lambda: self.manager.addZero())
+            for sval, succ_states in win_state_bucket.items():
+                pre_states: BDD = self.preimage(ts_action=partitioned_tr_bdd, From=succ_states)
+
+                if not pre_states.isZero():
+                    pre_buckets[sval + act_val] |= pre_states.toADD()       
+
+            # unions of all predecessors
+            pre_states: ADD = reduce(lambda x, y: x | y, pre_buckets.values())
+
+            # now take univ abstraction to remove edges to states with infinity value
+            upre_states: ADD = pre_states.univAbstract(self.iVars_cube)
+            tmp_strategy: ADD = upre_states.ite(self.manager.addZero(), self.manager.plusInfinity())
+
+            for sval, apre_s in pre_buckets.items():
+                # we skip the zero states
+                if sval != 0:
+                    tmp_strategy = tmp_strategy.max(apre_s.ite(self.manager.addConst(int(sval)), self.manager.addZero()))
             
+            new_tmp_strategy: ADD = tmp_strategy
+
+            # go over all the env actions and preserve the maximum one
+            for env_tr_dd in self.eAction_map.values():
+                new_tmp_strategy = new_tmp_strategy.max(tmp_strategy.restrict(self.cube_to_add(env_tr_dd, self.iVars)))
+
+            # compute the minimum of state action pairs
+            strategy = strategy.min(new_tmp_strategy)
+
+            self.winning_states[layer + 1] |= self.winning_states[layer]
+
+            for robot_tr_dd in self.rAction_map.values():
+                # remove the dependency for that action and preserve the minimum value for every state
+                self.winning_states[layer + 1] = self.winning_states[layer  + 1].min(strategy.restrict(self.cube_to_add(robot_tr_dd, self.oVars)))
+
             # update the counter
             layer += 1
 
 if __name__ == "__main__":
-    game = AddGridWorld(rows=2, columns=2, init = (0, 0), goal=(1, 1))
+    game = AddGridWorld(rows=4, columns=4, init = (0, 0), goal=(3, 3))
     game.create_transition_relation()
 
-    for var, f in game.transition_relation.items():
-        print(f"f_{var}: \n {f}")
+    # for var, f in game.transition_relation.items():
+    #     print(f"f_{var}: \n {f}")
+    
+    game.solve()
 
 
 
